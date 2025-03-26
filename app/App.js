d0795a0e151ad8a795cd76e72cb6e01a15d4981e
H82M19\Student <Student>
2025-03-26 15:38:54 +0700

    finish 1a

---
+++ app/App.js
@@ -0,0 +1,9 @@
+import { View , Text} from "react-native";
+
+export default function App(){
+    return (
+        <View style={{flex:1 , justifyContent: "center" , allignItems:"center"}}> 
+            <Text>Chao cac ban da den IUH!</Text>
+        </View>
+    )
+}
