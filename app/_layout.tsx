a308076801c548e9e736289d26b0728b85663a33
H82M19\Student <Student>
2025-03-26 15:52:23 +0700

    finish 1b

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,69 @@
 import React, { useEffect, useRef } from "react";
-import { View, Text, Animated } from "react-native";
+import { View, Animated } from "react-native";
 
 export default function App() {
-  const translateX = useRef(new Animated.Value(-200)).current;
+    const textX = useRef(new Animated.Value(-150)).current;
 
-  useEffect(() => {
-    Animated.timing(translateX, {
-      toValue: 0,
-      duration: 3000, 
-      useNativeDriver: true,
-    }).start();
-  }, []);
+    useEffect(() => {
+        Animated.loop(
+            Animated.sequence([
+                Animated.timing(textX, {
+                    toValue: 150,
+                    duration: 3000,
+                    useNativeDriver: true,
+                }),
+                Animated.timing(textX, {
+                    toValue: -150,
+                    duration: 3000,
+                    useNativeDriver: true,
+                }),
+            ])
+        ).start();
+    }, []);
 
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Animated.Text style={{ transform: [{ translateX }] }}>
-        Chào các bạn đã đến IUH!
-      </Animated.Text>
-    </View>
-  );
+    const circleX = useRef(new Animated.Value(200)).current;
+
+    useEffect(() => {
+        Animated.loop(
+            Animated.sequence([
+                Animated.timing(circleX, {
+                    toValue: -200,
+                    duration: 3000,
+                    useNativeDriver: true,
+                }),
+                Animated.timing(circleX, {
+                    toValue: 200,
+                    duration: 3000,
+                    useNativeDriver: true,
+                }),
+            ])
+        ).start();
+    }, []);
+
+    return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Animated.Text
+                style={{
+                    marginBottom: 50,
+                    fontSize: 18,
+                    fontWeight: "bold",
+                    transform: [{ translateX: textX }],
+                }}
+            >
+                Chào các bạn đã đến IUH!
+            </Animated.Text>
+
+            <Animated.View
+                style={{
+                    width: 50,
+                    height: 50,
+                    backgroundColor: "blue",
+                    borderRadius: 25,
+                    position: "absolute",
+                    bottom: 200,
+                    transform: [{ translateX: circleX }],
+                }}
+            />
+        </View>
+    );
 }
